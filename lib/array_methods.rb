26bf15ff27235997d8ac9f94800f9fc648b245ac
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-01 08:48:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,9 @@ array.include? ("Tardis")
 end
 
 def using_sort(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.sort ("wow")
+array.sort ("I")
 end
 
 def using_reverse(array)
